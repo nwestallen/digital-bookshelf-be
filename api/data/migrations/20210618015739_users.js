a561f52c870387288b8f964cf8cb8201341b4803
Nick <nwestallen@gmail.com>
2021-06-17 22:31:17 -0400

    adds knex scripts and cusers migration file

---
+++ api/data/migrations/20210618015739_users.js
@@ -0,0 +1,14 @@
+
+exports.up = (knex) => {
+  return knex.schema
+    .createTable('users', table => {
+      table.increments('user_id').notNullable().unique().primary();
+      table.string('user_name').notNullable().unique();
+      table.string('password').notNullable();
+      table.timestamps(true, true);
+    }); 
+};
+
+exports.down = knex =>  {
+  return knex.schema.dropTableIfExists('users');
+};
