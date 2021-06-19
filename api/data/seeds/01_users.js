bd69994dceecf68e49671b16bf3fc8ec8a96ab21
Nick <nwestallen@gmail.com>
2021-06-19 14:14:28 -0400

    creates users seed file

---
+++ api/data/seeds/01_users.js
@@ -0,0 +1,12 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(() => {
+      // Inserts seed entries
+      return knex('users').insert([
+        {user_name: 'nwestallen', password: 'asdflkjha'},
+        {user_name: 'chrissyboy', password: 'ffffffff'},
+      ]);
+    });
+};
