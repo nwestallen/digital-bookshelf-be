da83b6f618f682535e4122fa32b7bec106bb61f1
Nick <nwestallen@gmail.com>
2021-06-23 13:01:43 -0400

    adds tests for [GET] /api/books/:user_id/library

--- api/books/books.test.js
+++ api/books/books.test.js
@@ -17,7 +17,7 @@ it('sanity check', () => {
   expect(true).not.toBe(false);
 });
 
-describe('server.js', () => {
+describe('books', () => {
 
   describe('[GET] /api/books/:user_id', () => {
     it('[1] responds with status 200 when user_id exists', async () => {
@@ -34,6 +34,25 @@ describe('server.js', () => {
     });
     it('[4] responds with appropriate error message when user_id does not exist', async () => {
       const res = await request(server).get('/api/books/999');
+      expect(res.body.message).toMatch(/user_id 999 does not exist/);
+    });
+  });
+
+  describe('[GET] /api/books/:user_id/library', () => {
+    it('[5] responds with status 200 user_id exists', async () => {
+      const res = await request(server).get('/api/books/1/library');
+      expect(res.status).toBe(200);
+    });
+    it('[6] responds with nested library data when user_id exists', async () => {
+      const res = await request(server).get('/api/books/2/library');
+      expect(res.body[0]).toMatchObject({ case_id: 3 })
+    });
+    it('[7] responds with status 400 when user_id does not exist', async () => {
+      const res = await request(server).get('/api/books/999/library');
+      expect(res.status).toBe(400);
+    });
+    it('[8] responds with appropriate error message when user_id does not exist', async () => {
+      const res = await request(server).get('/api/books/999/library');
       expect(res.body.message).toMatch(/user_id 999 does not exist/);
     });
   });
