7ff0de5240e307c8c5575e0d8638dfdcb75811c7
Nick <nwestallen@gmail.com>
2021-06-23 10:13:08 -0400

    test passes sanity check

--- project.test.js
+++ project.test.js
@@ -1,2 +1,7 @@
 const request = require('supertest');
 const server = require('./api/server');
+const db = require('./data/db-config');
+
+it('sanity check', () => {
+  expect(true).not.toBe(false);
+});
